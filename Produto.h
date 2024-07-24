c3f89d346703891ca6951cf03950d6147a9bffa3
Tita <titahcarvalho@hotmail.com>
2024-07-24 20:26:42 -0300

    Envio do codigo do Produto.h

---
+++ Produto.h
@@ -0,0 +1,35 @@
+#ifndef PRODUTO_H
+#define PRODUTO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct produto{ //cria os quadradinhos
+    int codigo;
+    char descricao[100];
+    char nome[20];
+    int quantidade;
+    float preco;
+}TProduto;
+
+typedef struct celula{
+    TProduto item;      //utilizou o Tproduto antes declarado como variável para para declarar o item do tipo produto.
+    struct celula* prox;        // a célula cria um ponteiro chamado "prox"
+}TCelula;
+
+typedef struct{  //realizar a lista com os parametros a serem seguidos.
+    TCelula* primeiro;
+    TCelula* ultimo;
+    int tamanho;
+}TLista;
+
+//a partir daqui apenas o título, o corpo vai p o .C
+void FLVazia(TLista *Lista); //função para criar uma Lista //repare que aqui há a utilização do ponteiro devido a situação ser uma passagem por referencia onde há alterações
+int Vazia(TLista Lista); //Função que ira verificar se a lista esta vazia, repare que esá sem ponteiro, pois é uma passagem por valor sem alterações permanentes.
+     //se elas são iguais, apontam para o mesmo e está vazia, se não apontam p diferentes. 
+void Inserir(TProduto x, TLista *Lista);
+void Imprimir(TLista Lista);
+
+#endif
+    
